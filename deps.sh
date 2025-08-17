a6d2e70cd0118766884e70e2043e020cfea740fe
Steve Schnepp <steve.schnepp@pwkf.org>
2025-08-17 17:18:41 +0200

    update github dependencies

--- deps.sh
+++ deps.sh
@@ -1,7 +1,20 @@
 #! /bin/sh
 
+# Github pages plugins
 apt install \
 	jekyll \
 	jekyll-theme-minima \
+	ruby-jekyll-default-layout \
+	ruby-jekyll-optional-front-matter \
+	ruby-jekyll-paginate \
+	ruby-jekyll-readme-index \
+	ruby-jekyll-titles-from-headings \
 	ruby-jekyll-relative-links \
+
+# Additional plugins
+apt install \
+	ruby-jekyll-avatar \
+	ruby-jekyll-feed \
+	ruby-jekyll-redirect-from \
 	ruby-jekyll-seo-tag \
+	ruby-jekyll-sitemap \
